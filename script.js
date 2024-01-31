0292239b988e1b0254fcbab0e9b1430b76eb3188
Bruno Lobato <108697633+BrunoMLobato@users.noreply.github.com>
2024-01-31 01:11:27 -0300

    Galeria e Áudios Arrumados

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const botaoPlayPause = document.getElementById("play-pause");
 const botaoProximoCapitulo = document.getElementById("proximo");
 const botaoCapituloAnterior = document.getElementById("anterior");
 
-const quantidadeCapitulos = 10;
+
+let quantidadeCapitulos = 10;
 let taTocando = false;
 let capitulo = 1;
+let capituloAtual = 1;
+let Livro = 'Dom-Casmurro';
+
 
 function tocarFaixa() {
     botaoPlayPause.classList.remove("bi-play-circle-fill");
     botaoPlayPause.classList.add("bi-pause-circle-fill");
     audio.play();
     taTocando = true;
+}
+
+function trocarnomeFaixa() {
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual
 }
 
 function pausarFaixa() {
@@ -36,7 +44,7 @@ function capituloAnterior() {
     } else {
         capitulo -= 1;
     }
-    audio.src = "/books/Dom-Casmurro/" + "dom_casmurro_" + capitulo + "_machadodeassis" + ".mp3";
+    audio.src = "./books/" + Livro + "/" + capitulo + ".mp3";
     nomeCapitulo.innerText = "Capítulo " + capitulo;
     tocarFaixa();
 }
@@ -47,30 +55,56 @@ function proximoCapitulo() {
     } else {
         capitulo = 1;
     }
-    audio.src = "/books/Dom-Casmurro/" + "dom_casmurro_" + capitulo + "_machadodeassis" + ".mp3";
+    audio.src = "./books/" + Livro + "/" + capitulo + ".mp3";;
     nomeCapitulo.innerText = "Capítulo " + capitulo;
     tocarFaixa();
 }
 
-
-function atualizarCapaEAutor(capaUrl) {
-    const capaLivro = document.querySelector(".container-aplicativo img");
-    const nomeAutor = document.getElementById("nome-autor");
-
-    capaLivro.src = capaUrl;
-    nomeAutor.innerText = "Machado de Assis"; 
+// Função para atualizar o nome do autor
+function atualizarNomeAutor(novoNome) {
+    const nome_autor = document.getElementById("nome_autor");
+    nome_autor.textContent = novoNome;
 }
 
-document.querySelectorAll(".cards").forEach(card => {
-    card.addEventListener("click", function(event) {
-        event.preventDefault();
-        const capaUrl = this.querySelector("img").src;
-        atualizarCapaEAutor(capaUrl);
-    });
-});
+// Primeiro Card
+function PrimeiroCard() {
+    capitulo = 1
+    trocarnomeFaixa();
+    quantidadeCapitulos = 10;
+    atualizarNomeAutor('Machado de Assis');
+    const imagemLivro = document.getElementById('imagemLivro');
+    imagemLivro.src = './images/book_dom-casmurro.jpeg';
+    Livro = 'Dom-Casmurro';
+    audio.src = "./books/" + Livro + "/" + capitulo + ".mp3";;
+}
+
+// Segundo Card
+function SegundoCard() {
+    capitulo = 1
+    trocarnomeFaixa();
+    quantidadeCapitulos = 10;
+    atualizarNomeAutor('José de Alencar');
+    const imagemLivro = document.getElementById('imagemLivro');
+    imagemLivro.src = './images/book_cinco_minutos.jpg';
+    Livro = 'Cinco-Minutos';
+    audio.src = "./books/" + Livro + "/" + capitulo + ".mp3";;
+}
+
+// Terceiro Card
+function TerceiroCard() {
+    capitulo = 1
+    trocarnomeFaixa();
+    quantidadeCapitulos = 10;
+    atualizarNomeAutor('Autor 3');
+    const imagemLivro = document.getElementById('imagemLivro');
+    imagemLivro.src = './images/book_ressureicao.jpg';
+    Livro = 'Ressureicao';
+    audio.src = "./books/" + Livro + "/" + capitulo + ".mp3";;
+}
 
 botaoPlayPause.addEventListener("click", tocarOuPausarFaixa);
 botaoCapituloAnterior.addEventListener("click", capituloAnterior);
 botaoProximoCapitulo.addEventListener("click", proximoCapitulo);
 audio.addEventListener("ended", proximoCapitulo);
+
 
